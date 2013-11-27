b1712749ec9c7769eaf750918b8dff64019e72e8
Orrett Davis <orrett.davis@gmail.com>
2013-11-27 09:19:56 -0500

    Seeds.rb edits

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ActiveRecord::Base
-  attr_accessible :body, :references, :title
+  attr_accessible :body, :references, :title, :wiki
   belongs_to :wiki 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 require 'faker'
+require 'pry'
 
 wikis = []
 15.times do 
@@ -6,17 +7,16 @@ wikis = []
     topic: Faker::Lorem.words(rand(1..5)).join(" "),
     description: Faker::Lorem.paragraph(1))
 end
-
+#binding.pry 
 
 rand(4..10).times do 
   wiki = wikis.first
   a = wiki.articles.create(
     wiki: wiki,
     title: Faker::Lorem.words(rand(1..5)).join(" "),
-    body: Faker::Lorem.paragraph(rand(1.2)).join("\n"))
+    body: Faker::Lorem.paragraphs(rand(1..2)).join("\n"))
     a.update_attributes(:created_at, Time.now - rand(600..315360000))
     wikis.rotate!
-
 end
 
 
